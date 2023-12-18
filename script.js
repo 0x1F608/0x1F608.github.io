fd11d144d3eee8e118335631bc820e5878d378ce
Hex | 0x1F608 <143428679+0x1F608@users.noreply.github.com>
2023-12-18 22:49:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function load_youtube() {
+    window.open("https://www.youtube.com/channel/UCag7cztDCgGUYX0Vu6tuu6Am")
+}
+function load_github() {
+    window.open("https://github.com/0x1F608")
+}
+
+function load_discord() {
+    window.open("https://discord.gg/Yd2AvvESSK")
+}
+
+
+function grab_ip() {
+    const api = "https://api.ipify.org"
+    fetch(api)
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        console.log("This is not used for malicious purposes, I am just learning js: ", data);
+    })
+}
+
+onload(grab_ip())
